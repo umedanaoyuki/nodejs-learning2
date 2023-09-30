3b610b1c347a87e19b146ac5d5952830d80a010d
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-30 16:55:46 +0900

    練習問題

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -1,9 +1,12 @@
 import * as http from "http";
 
 const server = http.createServer(function (req, res){
-   console.log("成功");
    console.log(req.url);
-   res.end('Hello,World');
+   if (req.url === '/hello') {
+       res.end('bye');
+   } else {
+       res.end('hello');
+   }
 });
 
 server.listen(8080);
