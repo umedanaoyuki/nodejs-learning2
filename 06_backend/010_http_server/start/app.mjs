71ad077dc44f68080643f1ba0b66d515858e011e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-30 16:49:24 +0900

    nodemonインストール

--- 06_backend/010_http_server/start/app.mjs
+++ 06_backend/010_http_server/start/app.mjs
@@ -3,7 +3,7 @@ import * as http from "http";
 const server = http.createServer(function (req, res){
    console.log("成功");
    console.log(req.url);
-   res.end('hello');
+   res.end('Hello,World');
 });
 
 server.listen(8080);
